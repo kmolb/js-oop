61d72f5d3a6cc88693ae6f7ea76e029929274cbd
Kamil Olber <olb.kamil@gmail.com>
2018-10-27 10:33:32 +0200

    call 1.2

--- oop5.js
+++ oop5.js
@@ -2,14 +2,17 @@ function person(name, age)  {
     return{
     name: name,
     age: age,
-    print: function(){
-        console.log(this.name, this.age)
+    print: function(param1, param2){
+        console.log(this.name, this.age, param1, param2)
     }
 }}
 
 const persone1 = person("kamil", 31);
 const persone2 = person("tomek", 25);
-
+/*
 persone1.print(); // wywołanie funkcji 
+*/
+persone1.print.call(persone2, 'param1', 'param2');
 
-const result = persone2.print.call(persone2);
+persone1.print.call(persone2, ['param1', 'param2']);
+
